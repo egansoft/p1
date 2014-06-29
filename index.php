735376b390ecb819b810bd6d0f481c8b6c561d4f
Nicholas Egan <nicholas@egansoft.com>
2014-06-29 16:19:08 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Nicholas Egan P1</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    
+    <body>
+        <div id="wrap">
+            <h1 id="title">Nicholas Egan P1</h1>
+            <img src="images/picture.jpg" alt="A picture of Nicholas Egan" id="picture" />
+            
+            <h2>In 2+ sentences describe who you are and what you do.</h2>
+            <p>My name is Nicholas Egan. I'm a rising high school senior who lives in the San Francisco area and is attending 
+                the Harvard Summer School Program.</p>
+            
+            <h2>Why are you taking this course?</h2>
+            <p>I'm taking this course because I love programming and would like to learn more about making web applications 
+                with more advanced tools.</p>
+            
+            <h2>What previous experience do you have with HTML/CSS?</h2>
+            <p>I've been making websites for several years now, and have been building websites for clients. You can see
+                my portfolio at <a href="http://www.egansoft.com/">egansoft.com</a>.</p>
+            
+            <h2>What previous experience do you have with programming?</h2>
+            <p>I started programming when I was in fifth grade and it has been my passion ever since. I've been mostly
+                working with Java recently, and you can see a portfolio of android apps I have made at 
+                <a href="http://www.egansoft.com/">egansoft.com</a>.</p>
+            
+            <h2>Have you worked with PHP before? If so, to what extent?</h2>
+            <p>I have worked with PHP a bit in the past, through initially reading a beginer's book and later through
+                googling. I don't however have a very strong foundation in PHP and would like to become better at it
+                through this class.</p>
+            
+            <h2>Are you working on a Mac, PC, or some other platform?</h2>
+            <p>PC with Windows 8.1</p>
+            
+            <h2>Anything else we should know?</h2>
+            <p>No</p>
+        </div>
+    </body>
+</html>
